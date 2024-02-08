573a87d94e859da0e6199ea7e08fa5dbf17f359f
Hemmie001 <emmanuelakej@gmail.,com>
2024-02-08 21:44:06 +0000

    All

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module implements a hash_password function 
+that expects one string argument name password and
+returns a salted, hashed password, which is a byte string
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """This function ashes a password using a random salt"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    This function 'Checks' is a hashed password
+    formed from the given password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
